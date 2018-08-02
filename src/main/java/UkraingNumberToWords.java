ee8ed6a69889c75715dca27dddf8b86a43bd5903
YatsunAnna1 <anny0-2011@mail.ru>
2018-08-02 13:08:41 +0200

    commit try to refactor clasess

--- src/main/java/UkraingNumberToWords.java
+++ src/main/java/UkraingNumberToWords.java
@@ -1,104 +1,114 @@
-public class UkraingNumberToWords extends AllInformation {
-    public static String convertLessThanOneThousand(int number) {
- 
 
-        if (number % 100 < 20){
-            AllInformation.stringNumber = numNamesUkr[number % 100];
-            number /= 100;
+public class UkraingNumberToWords extends ConvertLessThanOneThousand {
+    public String convert(long number) {
+        if (number == 0) {
+            return "нуль";
         }
-        else {
-            AllInformation.stringNumber = numNamesUkr[number % 10];
-            number /= 10;
-
-            AllInformation.stringNumber = tensNamesUkr[number % 10] + AllInformation.stringNumber;
-            number /= 10;
-        }
-        if (number == 0) return AllInformation.stringNumber;
-        return numNames2Ukr[number]  + stringNumber;
+        convertin(number);
+        String result = getStringMiliard(getBillions());
+        result = getStringMilion(getMillions(), result);
+        result = getStringThouthend(getThousands(), result);
+        String tradThousand;
+        tradThousand = convertLessThanOneThousandUkr(getThousands());
+        result = result + tradThousand;
+        return result.replaceAll("^\\s+", "").replaceAll("\\b\\s{2,}\\b", " ");
     }
 
-    public static String getStringBilions(int billions) {
+    private String getStringMiliard(int billions) {
+        String tradBillions;
+        String miliard = " мiльярд";
+        String sufixMiliard = "и";
+        String sufixMiliard2 = "iв";
         switch (billions) {
             case 0:
                 tradBillions = "";
                 break;
-            case 1 :
-                tradBillions = convertLessThanOneThousand(billions)
+            case 1:
+                tradBillions = convertLessThanOneThousandUkr(billions)
                         + miliard;
                 break;
-            case 2 :
-                tradBillions = convertLessThanOneThousand(billions)
-                        + miliard+sufixMiliard;
+            case 2:
+                tradBillions = convertLessThanOneThousandUkr(billions)
+                        + miliard + sufixMiliard;
                 break;
-            case 3 :
-                tradBillions = convertLessThanOneThousand(billions)
-                        + miliard+sufixMiliard;
+            case 3:
+                tradBillions = convertLessThanOneThousandUkr(billions)
+                        + miliard + sufixMiliard;
                 break;
-            case 4 :
-                tradBillions = convertLessThanOneThousand(billions)
-                        + miliard+sufixMiliard;
+            case 4:
+                tradBillions = convertLessThanOneThousandUkr(billions)
+                        + miliard + sufixMiliard;
                 break;
-            default :
-                tradBillions = convertLessThanOneThousand(billions)
-                        +miliard+sufixMiliard2;
+            default:
+                tradBillions = convertLessThanOneThousandUkr(billions)
+                        + miliard + sufixMiliard2;
         }
         return tradBillions;
     }
 
-    public static String getStringMilions(int millions, String result) {
-        switch (millions) {
-            case 0:
-                tradMillions = "";
-                break;
-            case 1 :
-                tradMillions = convertLessThanOneThousand(millions)
-                        +million;
-                break;
-            case 2 :
-                tradMillions = convertLessThanOneThousand(millions)
-                        +million+sufictToString;
-                break;
-            case 3 :
-                tradMillions = convertLessThanOneThousand(millions)
-                        +million+sufictToString;
-                break;
-            case 4 :
-                tradMillions = convertLessThanOneThousand(millions)
-                        +million+sufictToString;
-                break;
-
-            default :
-                tradMillions = convertLessThanOneThousand(millions)
-                        +million+sufictToString2;
-        }
-        result =  result + tradMillions;
-        return result;
-    }
-    public static String getStringThousand(int hundredThousands, String result) {
-
+    public String getStringThouthend(int hundredThousands, String result) {
+        String tradHundredThousands;
+        String southend = "тисяч";
+        String sufictToStringsouth = "a";
+        String sufictToStringsouth1 = "i";
         switch (hundredThousands) {
             case 0:
                 tradHundredThousands = "";
                 break;
-            case 1 :
-                tradHundredThousands = "oдна"+southend+sufictToStringsouth;
+            case 1:
+                tradHundredThousands = "oдна" + southend + sufictToStringsouth;
                 break;
-            case 2 :
-                tradHundredThousands ="двi "+southend+sufictToStringsouth1;
+            case 2:
+                tradHundredThousands = "двi " + southend + sufictToStringsouth1;
                 break;
-            case 3 :
-                tradHundredThousands = "три "+southend+sufictToStringsouth1;
+            case 3:
+                tradHundredThousands = "три " + southend + sufictToStringsouth1;
                 break;
-            case 4 :
-                tradHundredThousands = "чотири "+southend+sufictToStringsouth1;  ;
+            case 4:
+                tradHundredThousands = "чотири " + southend + sufictToStringsouth1;
+                ;
                 break;
-            default :
-                tradHundredThousands = convertLessThanOneThousand(hundredThousands)
+            default:
+                tradHundredThousands = convertLessThanOneThousandUkr(hundredThousands)
                         + " тисяч ";
         }
-        result =  result + tradHundredThousands;
+        result = result + tradHundredThousands;
         return result;
     }
 
+    private String getStringMilion(int millions, String result) {
+        String tradMillions;
+        String million = " мiльйон";
+        String sufictToString = "и";
+        String sufictToString2 = "iв";
+        switch (millions) {
+            case 0:
+                tradMillions = "";
+                break;
+            case 1:
+                tradMillions = convertLessThanOneThousandUkr(millions)
+                        + million;
+                break;
+            case 2:
+                tradMillions = convertLessThanOneThousandUkr(millions)
+                        + million + sufictToString;
+                break;
+            case 3:
+                tradMillions = convertLessThanOneThousandUkr(millions)
+                        + million + sufictToString;
+                break;
+            case 4:
+                tradMillions = convertLessThanOneThousandUkr(millions)
+                        + million + sufictToString;
+                break;
+            default:
+                tradMillions = convertLessThanOneThousandUkr(millions)
+                        + million + sufictToString2;
+        }
+        result = result + tradMillions;
+        return result;
+    }
 
 }
+
+
